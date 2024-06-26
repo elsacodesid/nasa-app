f6f9d9968d6f0621facd4bd232cf839af1345871
elsacodesid <elsacodesid@gmail.com>
2024-06-26 08:51:18 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
+      <div className="bgGradient"></div>
       <div>
         <h2>Futuristic rice field</h2>
         <h1>APOD PROJECT</h1>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div><h2>Futuristic Rice Field</h2>
-  <div>Description</div>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima minus, corrupti dolores laborum fuga dolor.</p>
-  </div>;
+  return (
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>Futuristic Rice Field</h2>
+        <div>Description</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+          minus, corrupti dolores laborum fuga dolor.
+        </p>
+        <button>
+        <i className="fa solid fa-arrow-right"></i>
+      </button>
+      </div>
+
+    </div>
+  );
 };
 
 export default SideBar;
