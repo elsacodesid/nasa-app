e71f4478ad5b13df57326c701833ad7c52e91e13
elsacodesid <elsacodesid@gmail.com>
2024-06-27 09:43:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,17 @@ function App() {
   }
 
   useEffect(() => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
       const url =
         "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
       try {
         const res = await fetch(url);
-        const data = await res.json();
-        console.log("DATA\n", data);
+        const apiData = await res.json();
+        setData(apiData)
+        console.log("DATA\n", apiData);
       } catch (error) {
         console.log(error.message);
       }
